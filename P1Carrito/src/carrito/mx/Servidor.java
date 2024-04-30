19e1c6d9efa3fa557a24ab9e6d76c214d7393862
Saúl Hernández <96091580+SaulHdzA17@users.noreply.github.com>
2024-04-30 10:19:18 -0600

    server

--- P1Carrito/src/carrito/mx/Servidor.java
+++ P1Carrito/src/carrito/mx/Servidor.java
@@ -1,6 +1,8 @@
 package carrito.mx;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Servidor {
@@ -24,16 +26,69 @@ public class Servidor {
                 Socket cl = s.accept();//"s" acepta la conexión con accept() y devuelve un objeto Socket "cl" que representa la conexión
                 System.out.println("Conexion establecida desde: " + cl.getInetAddress() + ":" + cl.getPort());//Imprime la dirección IP y
                 //puerto del cliente que se ha conectado al servidor
-                String mensaje = "Nombre completo: Hernandez Alonso Saul. Grupo: 6CM4. Materia: Aplicaciones para Comunicaciones en Red.";//Creamos el mensaje que enviaremos al cliente.
+                String mensaje = "Conexión establecida al servidor";//Creamos el mensaje que enviaremos al cliente.
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));//Usamos PrintWriter para un flujo de salida,
                 pw.println(mensaje);//en este caso lo vamos a ligar al mensaje que escribimos en el Servidor para enviarlo y mostrarlo 
                 pw.flush();//en la consola del Cliente, limpiamos el flujo.
 
+                String rutaArchivo = "C:\\Users\\raygu\\OneDrive\\Desktop\\Redes2\\Redes_2\\P1Carrito\\src\\carrito\\archivos\\productos.txt";
+                serializarArchivo(rutaArchivo);
+               
                 pw.close();//Cerramos el objeto pw.
                 cl.close();//Cerramos el socket del Cliente creado previamentex
             }//Cerramos bucle for
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public static void serializarArchivo(String ruta) {//Método para verificar que el archivo que contiene el catalogo de productos existe y se 
+        File archivo = new File(ruta);
+        if (archivo.exists()) {
+            StringBuilder content = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                System.out.println("Contenido del archivo:");
+                System.out.println(content.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("El archivo no existe.");
+        }
+        
+        
+        //Guardado
+        ArrayList <String> arg = guardarElementos(ruta);
+         
+    }
+    
+    public static ArrayList<String> guardarElementos(String filePath) {
+        ArrayList<String> fileLines = new ArrayList<>();
+        File file = new File(filePath);
+        
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    fileLines.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("El archivo no existe.");
+            return null;
+        }
+        
+        if (fileLines.isEmpty()) {
+            System.out.println("El archivo está vacío.");
+            return null;
+        }
+        
+        return fileLines;
     }
 }
