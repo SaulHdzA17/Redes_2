d4d1d884ccc09194ccc054818c3f398dc49213d3
GibhramCR <gibhramc@gmail.com>
2024-05-13 22:23:33 -0600

    man vergaz del server

--- P1Carrito/src/carrito/mx/Servidor.java
+++ P1Carrito/src/carrito/mx/Servidor.java
@@ -6,61 +6,64 @@ import java.util.Scanner;
 
 public class Servidor {
     
-    public static void main(String[] args) {
-        String scan = new String();
-        Scanner sc = new Scanner(System.in);
-        Catalogo producto = new Catalogo();
-        ArrayList<Catalogo> catalogo = new ArrayList<>();
-    
-        try {
-            System.out.println("*****Iniciando servidor*****");
-            System.out.println("Ingrese el puerto a utilizar:");
-            scan = sc.nextLine();
-            System.out.println("Puerto " + scan + " ingresado");
-            System.out.println("Validando conexión");
-    
-            ServerSocket s = new ServerSocket(Integer.parseInt(scan));
+   public static void main(String[] args) {
+    String scan;
+    Scanner sc = new Scanner(System.in);
+    Catalogo producto;
+    ArrayList<Catalogo> catalogo;
+
+    try {
+        System.out.println("*****Iniciando servidor*****");
+        System.out.println("Ingrese el puerto a utilizar:");
+        scan = sc.nextLine();
+        System.out.println("Puerto " + scan + " ingresado");
+        System.out.println("Validando conexión");
+
+        try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(scan))) {
             System.out.println("Esperando cliente...");
-    
+
             // Mantener el servidor en un bucle infinito para recibir múltiples conexiones de clientes
             while (true) {
-                Socket cl = s.accept();
-                System.out.println("Conexion establecida desde: " + cl.getInetAddress() + ":" + cl.getPort());
-    
-                //String rutaArchivo = "C:\\Users\\raygu\\OneDrive\\Desktop\\Redes2\\Redes_2\\P1Carrito\\src\\carrito\\archivos\\productos.txt";
-                String rutaArchivo = "C:\\Users\\dra55\\Documents\\GitHub\\Redes_2\\P1Carrito\\src\\carrito\\archivos\\productos.txt";
-                ArrayList<String> atributos = Servidor.obtenerContenidoTxt(rutaArchivo);
-    
-                for (int x = 0; x < atributos.size(); x++) {
-                    producto = Catalogo.obtnerAtributos(atributos.get(x));
-                    catalogo.add(producto);
+                try (Socket cl = serverSocket.accept()) {
+                    System.out.println("Conexion establecida desde: " + cl.getInetAddress() + ":" + cl.getPort());
+
+                    String rutaArchivo = "C:\\Users\\dra55\\Documents\\GitHub\\Redes_2\\P1Carrito\\src\\carrito\\archivos\\productos.txt";
+                    ArrayList<String> atributos = Servidor.obtenerContenidoTxt(rutaArchivo);
+
+                    catalogo = new ArrayList<>();
+                    for (String atributo : atributos) {
+                        producto = Catalogo.obtnerAtributos(atributo);
+                        catalogo.add(producto);
+                    }
+
+                    byte[] arg = Catalogo.serializarLista(catalogo);
+
+                    // Obtener flujo de salida del socket y enviar los datos serializados al cliente
+                    try (OutputStream outputStream = cl.getOutputStream()) {
+                        outputStream.write(arg);
+                        outputStream.flush();
+                    }
+
+                    // Esperar a que el cliente envíe los datos actualizados
+                    try (InputStream inputStream = cl.getInputStream()) {
+                        arg = inputStream.readAllBytes();
+                    }
+
+                    // Limpiar y actualizar el catálogo
+                    catalogo.clear();
+                    catalogo = Catalogo.deserializarLista(arg);
+                    Servidor.guardadArchivo(rutaArchivo, catalogo);
                 }
-    
-                byte[] arg = Catalogo.serializarLista(catalogo);
-    
-                // Obtener flujo de salida del socket y enviar los datos serializados al cliente
-                OutputStream outputStream = cl.getOutputStream();
-                outputStream.write(arg);
-                outputStream.flush();
-                                
-                // Esperar a que el cliente envíe los datos actualizados
-                InputStream inputStream = cl.getInputStream();
-                arg = inputStream.readAllBytes();
-                
-                // Limpiar y actualizar el catálogo
-                catalogo.clear();
-                catalogo = Catalogo.deserializarLista(arg);
-                Servidor.guardadArchivo(rutaArchivo, catalogo);
-                // Cerrar flujos y sockets
-                //outputStream.close();
-                inputStream.close();
-                cl.close();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        sc.close();
     }
-    
+}
+ 
     
     public static ArrayList <String> obtenerContenidoTxt(String ruta) {//Con este método obtenemos el contenido del txt y guardamos línea por línea
         File archivo = new File(ruta);//el contenido en un elemento de
